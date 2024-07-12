66e822913a8d11874cb4cafc819ffb17320ba336
Harsh <harshtanishq2002@gmail.com>
2024-07-12 15:09:23 +0530

    feat: rendering lottery contract

--- src/App.js
+++ src/App.js
@@ -4,12 +4,26 @@ import web3 from "./web3";
 import lottery from "./lottery";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      manager: "",
+    };
+  }
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+
+    this.setState({ manager });
+  }
+
   render() {
     web3.eth.getAccounts().then(console.log);
 
     return (
-      <div className="App">
-        <h1>Hello!</h1>
+      <div>
+        <h2>lottery contract</h2>
+        <h3>this contract managed by {this.state.manager}</h3>
       </div>
     );
   }
